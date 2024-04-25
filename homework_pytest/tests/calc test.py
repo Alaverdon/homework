9184165bb75dc95bddc153b9b3caeb25b8c3cb10
Alaverdon <158495791+Alaverdon@users.noreply.github.com>
2024-04-25 21:49:44 +0400

    Add files via upload

---
+++ homework_pytest/tests/calc test.py
@@ -0,0 +1,24 @@
+
+from app.calc import Calculator
+
+
+class TestCalc:
+
+    def setup(self):
+        self.calc = Calculator
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 1, 1) == 2
+
+
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_division_success(self):
+        assert self.calc.division(self, 2, 2) == 1
+
+    def test_subtract_success(self):
+        assert self.calc.subtraction(self, 4, 3) == 1
+
+    def teardown(self):
+        print("Выполнение метода Teardown")
